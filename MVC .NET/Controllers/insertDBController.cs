c4b043973fec05ba499bab0393ec6f39344165f5
sanke <sanke@MSI>
2025-02-04 09:57:00 +0530

    Add project files.

---
+++ MVC .NET/Controllers/insertDBController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.NET.Models;
+
+namespace MVC.NET.Controllers
+{
+    public class insertDBController : Controller
+    {
+        MVCREGEntities dbob = new MVCREGEntities();
+        public ActionResult Insert_pageload()
+        {
+            return View();
+        }
+        public ActionResult Insert_click(INSERTCS objcls)
+        {
+            if(ModelState.IsValid)
+            {
+                dbob.sp_testtabinsert(objcls.name,objcls.age,objcls.address);
+                objcls.msg = "inserted";
+                return View("Insert_pageload",objcls);
+            }
+            else
+            {
+                objcls.msg = "not inserted";
+                return View("Insert_pageload", objcls);
+            }
+          
+        }
+    }
+}
---
+++ MVC .NET/Models/INSERTCS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.NET.Models
+{
+    public class INSERTCS
+    {
+        [Required(ErrorMessage ="Enter the name")]
+        public string name { set; get; }
+        [Range(20,60, ErrorMessage = "Enter the age between 20 to 60")]
+        public int age { set; get; }
+        [Required(ErrorMessage = "Enter the address")]
+        public string address { set; get; }
+        public string msg { set; get; }
+    }
+}
